c6626ccefa81cfe4b50a9a5fcc459918d0794b99
Naser Sami <ponanzar@gmail.com>
2022-03-04 22:14:27 +0300

    MessageLine + StramBuilder

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+final _firestore = FirebaseFirestore.instance;
+// form firebase User
+late User signedInUser;
+
 class ChatScreen extends StatefulWidget {
   static const String screenRoute = 'chat_screen';
 
@@ -12,10 +16,9 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  final _firestore = FirebaseFirestore.instance;
+  final messageTextController = TextEditingController();
+
   final _auth = FirebaseAuth.instance;
-  // form firebase User
-  late User signedInUser;
   String? messageText;
 
   @override
@@ -63,89 +66,199 @@ class _ChatScreenState extends State<ChatScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.yellow[900],
-        title: Row(
-          children: [
-            Image.asset(
-              "assets/logo.png",
-              height: 25,
-            ),
-            const SizedBox(width: 10),
-            const Text(
-              "MessageMe",
-              style: TextStyle(),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.yellow[900],
+          title: Row(
+            children: [
+              Image.asset(
+                "assets/logo.png",
+                height: 25,
+              ),
+              const SizedBox(width: 10),
+              const Text(
+                "MessageMe",
+                style: TextStyle(),
+              ),
+            ],
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                // add logout function
+                _auth.signOut();
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.close,
+              ),
             ),
           ],
         ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              // add logout function
-              _auth.signOut();
-              Navigator.pop(context);
-            },
-            icon: const Icon(
-              Icons.close,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const MessageStreamBuilder(),
+              Container(
+                decoration: const BoxDecoration(
+                  border: Border(
+                    top: BorderSide(
+                      color: Colors.orange,
+                      width: 2,
+                    ),
+                  ),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: messageTextController,
+                        onChanged: ((value) {
+                          messageText = value;
+                        }),
+                        decoration: const InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 20,
+                          ),
+                          hintText: "Write your message here...",
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        messageTextController.clear();
+                        _firestore.collection('messages').add({
+                          'text': messageText,
+                          'sender': signedInUser.email,
+                        });
+                      },
+                      child: Text(
+                        "send",
+                        style: TextStyle(
+                          color: Colors.blue[800],
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MessageStreamBuilder extends StatelessWidget {
+  const MessageStreamBuilder({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return // display these messages in our app
+        // StreamBuilder Widget
+
+        StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection('messages').snapshots(),
+      builder: (context, snapshots) {
+        List<MessageLine> messageWidgets = [];
+
+        if (!snapshots.hasData) {
+          return const Center(
+            child: CircularProgressIndicator(
+              color: Colors.blue,
+            ),
+          );
+        }
+
+        final messages = snapshots.data!.docs;
+        for (var message in messages) {
+          final messageText = message.get('text');
+          final messageSender = message.get('sender');
+          final currentUser = signedInUser.email;
+
+          final messageWidget = MessageLine(
+            text: messageText,
+            sender: messageSender,
+            isMe: currentUser == messageSender,
+          );
+          messageWidgets.add(messageWidget);
+        }
+
+        return Expanded(
+          child: ListView(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 20,
+            ),
+            children: messageWidgets,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class MessageLine extends StatelessWidget {
+  final String? sender;
+  final String? text;
+  final bool isMe;
+
+  const MessageLine({this.sender, this.text, required this.isMe, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Text(
+            '$sender',
+            style: TextStyle(
+              fontSize: 12,
+              color: Colors.yellow[900],
+            ),
+          ),
+          Material(
+            color: isMe ? Colors.blue[800] : Colors.white,
+            elevation: 5,
+            borderRadius: isMe
+                ? const BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  )
+                : const BorderRadius.only(
+                    topRight: Radius.circular(30),
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text(
+                '$text',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: isMe ? Colors.white : Colors.black45,
+                ),
+              ),
             ),
           ),
         ],
-      ),
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(),
-            Container(
-              decoration: const BoxDecoration(
-                border: Border(
-                  top: BorderSide(
-                    color: Colors.orange,
-                    width: 2,
-                  ),
-                ),
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: TextField(
-                      onChanged: ((value) {
-                        messageText = value;
-                      }),
-                      decoration: const InputDecoration(
-                        contentPadding: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 20,
-                        ),
-                        hintText: "Write your message here...",
-                        border: InputBorder.none,
-                      ),
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      _firestore.collection('messages').add({
-                        'text': messageText,
-                        'sender': signedInUser.email,
-                      });
-                    },
-                    child: Text(
-                      "send",
-                      style: TextStyle(
-                        color: Colors.blue[800],
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
